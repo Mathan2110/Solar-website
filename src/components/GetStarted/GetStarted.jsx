8f890c60e0f5a72f1adbe2aa465490301e64c2cb
Mathan2110 <maddy5410y@gmail.com>
2025-07-14 19:21:48 +0530

    solved overflow x in ui in services component

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -8,7 +8,7 @@ const GetStarted = () => {
         <h3>Get Started</h3>
         <h1>Start Your Solar Journey Today</h1>
         <p> Make the switch to clean, renewable energy today. Whether you're a homeowner, business, or community organization, our expert team is here to guide you every step of the way</p>
-        <Link to={'/contact'} ><button className='getstarted_btn'>Contact Us</button></Link>
+        <Link to={'/contact'} className='anchor' ><button className='getstarted_btn'>Contact Us</button></Link>
     </div>
   )
 }
