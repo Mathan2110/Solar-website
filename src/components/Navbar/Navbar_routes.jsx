32e0b738f9a8b2aa8810c3df1adcd066b3482e60
Mathan2110 <maddy5410y@gmail.com>
2025-07-14 18:23:50 +0530

    included touch any where to hide menu to navbar

--- src/components/Navbar/Navbar_routes.jsx
+++ src/components/Navbar/Navbar_routes.jsx
@@ -1,6 +1,5 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState,useEffect,useRef } from 'react'
 import { Link } from 'react-router-dom';
-import AnchorLink from 'react-anchor-link-smooth-scroll'
 import menu_icon from '../../assets/icons/menu-icon.svg'
 import close_icon from '../../assets/icons/close-icon.svg'
 import './Navbar.css'
@@ -9,6 +8,13 @@ import logo from '../../assets/logo.png'
 const Navbar_routes = () => {
     const [openMenu,setOpenMenu] = useState(false)
     const [isSticky, setIsSticky] = useState(false);
+    const menuRef = useRef(null);
+
+    const handleClickOutside = (event) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setOpenMenu(false);
+        }
+    };
 
     useEffect(() => {
         const handleScroll = () => {
@@ -16,7 +22,14 @@ const Navbar_routes = () => {
         };
 
         window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
+
+        document.addEventListener('mousedown', handleClickOutside);
+        document.addEventListener('touchstart', handleClickOutside);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+            document.removeEventListener('mousedown', handleClickOutside);
+            document.removeEventListener('touchstart', handleClickOutside);
+        }
     }, []);
 
   return (
@@ -27,7 +40,7 @@ const Navbar_routes = () => {
             <div className="menu_icon" onClick={()=>setOpenMenu(true)}>
                 <img src={menu_icon} alt="" />
             </div>
-            <ul className={openMenu ? 'navlinks open_menu' : 'navlinks close_menu'}>
+            <ul ref={menuRef} className={openMenu ? 'navlinks open_menu' : 'navlinks close_menu'}>
                 <img src={close_icon} alt="" className='close_icon' onClick={()=>setOpenMenu(false)} />
                 <Link to={'/'} className='anchor' ><li>Home</li></Link>
                 <Link to={'/'} className='anchor' ><li>About</li></Link>
